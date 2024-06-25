81a63bde24d7971049196b5ee63434061ded395f
rayne <rayne.jurchen@gmail.com>
2024-06-25 23:56:46 +0200

    add working gradient progress

--- 04-counter/script.js
+++ 04-counter/script.js
@@ -6,33 +6,42 @@ let count = 0;
 btn.addEventListener("click", function () {
   count = 0;
   main.innerText = count;
+  progressColorGradient();
 });
 
 main.addEventListener("click", function () {
-  clickCounter();
+  countClicks();
+  progressColorGradient();
 });
 
 window.addEventListener("keydown", function (event) {
   if (event.keyCode === 32 || event.keyCode === 13) {
-    clickCounter();
+    countClicks();
+    progressColorGradient();
   }
 });
 
-function clickCounter() {
-  //   let part1 = 100 - count;
-  //   let part2 = count;
-  //   let gradient =
-  //     "rgba (0,0,0,0) linear-gradient(270deg, rgb(33,33,33)" +
-  //     part1 +
-  //     "%" +
-  //     ", rgb(11,115,105)" +
-  //     part2 +
-  //     "%" +
-  //     ") repeat scroll 0% 0% / auto padding-box border-box";
+function countClicks() {
   count++;
   main.innerText = count;
-  //   console.log(part1);
-  //   console.log(part2);
-  //   console.log(gradient);
-  // style.background = gradient;
+}
+
+function progressColorGradient() {
+  let color2 = "rgba(33, 33, 33, 1)";
+  let color1 = "rgba(11, 115, 105, 1)";
+  let gradient = "";
+  let part = count % 100;
+
+  gradient =
+    "linear-gradient(to right," +
+    color1 +
+    part +
+    "%" +
+    "," +
+    color2 +
+    part +
+    "%" +
+    ")";
+
+  body.style.background = gradient;
 }
