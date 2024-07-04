af21fd8e6b72661dbb629041c8431a7004703bb4
rayne <rayne.jurchen@gmail.com>
2024-07-04 14:24:50 +0200

    optimize hexcode, add css only slider progress color

--- 05-color-mixer/script.js
+++ 05-color-mixer/script.js
@@ -18,11 +18,19 @@ function changeBackground() {
   let red = slider1.value;
   let green = slider2.value;
   let blue = slider3.value;
-  p.innerText =
-    "#" +
-    parseInt(red).toString(16) +
-    parseInt(green).toString(16) +
-    parseInt(blue).toString(16);
+  let hexR = parseInt(red).toString(16);
+  let hexG = parseInt(green).toString(16);
+  let hexB = parseInt(blue).toString(16);
+  if (hexR.length === 1) {
+    hexR = "0" + hexR;
+  }
+  if (hexG.length === 1) {
+    hexG = "0" + hexG;
+  }
+  if (hexB.length === 1) {
+    hexB = "0" + hexB;
+  }
+  p.innerText = "#" + hexR + hexG + hexB;
   body.style.background = "rgba(" + red + "," + green + "," + blue + ")";
 }
 
